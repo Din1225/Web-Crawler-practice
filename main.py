71c9b0dae683221f7afdbdc8d380fce27a7f2499
Din <109422290+Din1225@users.noreply.github.com>
2022-08-23 07:32:45 +0800

    GUI added

--- main.py
+++ main.py
@@ -2,13 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import xlwings as xw
+import tkinter as tk
+import tkinter.ttk as ttk
+import time
 
-#開啟excel
-workbook = xw.Book()
+#照地點排序好，最後存檔
+def analyze():
+    global city
+    wb = xw.Book(f"{city}展覽資訊.xlsx")
+    df = pd.read_excel(f"{city}展覽資訊.xlsx",sheet_name=f"{city}")
+    df_id = df.set_index("展覽名稱")
 
-#爬台北、台南、高雄展覽的資料
-cities = ["taipei","tainan","kaohsiung"]
-for city in cities :
+    df_id["展覽時間"] = df_id["展覽時間"].str.replace("\n"," ")
+    df_id["展覽地點"] = df_id["展覽地點"].str.replace("\n"," ")
+    df_id["票價資訊"] = df_id["票價資訊"].str.replace("_x000D_\n\t\t\t\t"," ")
+
+    df_bysite = df_id.sort_values(by=["展覽地點"])
+    # print(df_bysite)
+    sht = wb.sheets[f"{city}"]
+    sht.range("A1").value = df_bysite
+    sht.range("A:D").autofit()
+    wb.save(f"{city}展覽資訊.xlsx")
+    #wb.close()
+
+#讓使用者選擇爬台北、台南或高雄展覽的資料
+def choice():
+    return mycombobox.get()
+
+#按下按鈕後開始爬+整理資料
+def button_event():
+    global city
+    city = choice()
+    time.sleep(1)
+    find_data()
+    analyze()
+
+def find_data():
+    #選好後，開始爬資料
+    global city
     res = requests.get(f"https://www.funtime.com.tw/localtour/topic/index.php?theme=taiwan-exhibition&target={city}")
     res.encoding="UTF-8"
     html = BeautifulSoup(res.text,"html.parser")
@@ -17,7 +48,8 @@ for city in cities :
     descriptions = html.findAll("div",{"class":"type_c_sub_description"})
 
     #操作excel
-    workbook.sheets["工作表1"].name.replace("工作表1",f"{city}")
+    workbook = xw.Book()
+    #workbook.sheets["工作表1"].name.replace("工作表1",f"{city}")
     sheet = workbook.sheets.add(name=f'{city}', before='工作表1')
     sheet.range("A1:D1").value = ["展覽名稱","展覽時間","展覽地點","票價資訊"]
 
@@ -30,7 +62,6 @@ for city in cities :
     #(debug)
     #print(descriptions[1].text[103])
     #print(len(descriptions[1].text))
-
 
     #寫入展覽的詳細資料
     #temp1紀錄展覽票價的index temp2紀錄展覽地點的index
@@ -64,8 +95,22 @@ for city in cities :
     #整理格式
     sheet.range("A:D").column_width = 23
     sheet.range("A:D").autofit()
+    workbook.save(f"{city}展覽資訊.xlsx")
+    #workbook.close()
+    
+#使用者介面
+root = tk.Tk()
+root.title("Search-Exhibition")
+root.geometry("250x150")
 
-workbook.save("展覽資訊.xlsx")
-workbook.close()
+mylabel = tk.Label(root, text='請選擇你要查詢的城市')
+mylabel.place(x = 125,y = 30,anchor="center")
 
+mycombobox = ttk.Combobox(root, state="readonly",values=["taipei","tainan","kaohsiung"])
+mycombobox.pack(pady=50)
+mycombobox.current(0)
 
+mybutton = tk.Button(root, text='確定',command=button_event)
+mybutton.place(x = 125,y = 90,anchor="center")
+
+root.mainloop()
